71ca4736dae7fab8223782ec162d23034086edfc
Brundesh <brundesh@datasemantics.in>
2017-01-30 12:17:15 +0530

    Update sparkstreaming.py

--- pyspark/streaming/sparkstreaming.py
+++ pyspark/streaming/sparkstreaming.py
@@ -1,33 +1,32 @@
+
+"""
+ Counts words in UTF8 encoded, '\n' delimited text received from the network every second.
+ Usage: network_wordcount.py <hostname> <port>
+   <hostname> and <port> describe the TCP server that Spark Streaming would connect to receive data.
+ To run this on your local machine, you need to first run a Netcat server
+    `$ nc -lk 9999`
+ and then run the example
+    `$ bin/spark-submit examples/src/main/python/streaming/network_wordcount.py localhost 9999`
+"""
 from __future__ import print_function
 
 import sys
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
 
-
-#This piece of code is used to check the arguments for the program
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: kafka_wordcount.py <zk> <topic>", file=sys.stderr)
+        print("Usage: network_wordcount.py <hostname> <port>", file=sys.stderr)
         exit(-1)
-#create spark context
-    sc = SparkContext(appName="PythonStreamingKafkaWordCount")
-#Create streaming context and second arguments is the time interval specified for creating the batches and process them
-  ssc = StreamingContext(sc, 10)
+    sc = SparkContext(appName="PythonStreamingNetworkWordCount")
+    ssc = StreamingContext(sc, 1)
 
-
-#assigning command line arguments to variables for zookeeper and  kafka topic name
-    zkQuorum, topic = sys.argv[1:]
-# using KafkaUtils class we can connect to specified kafka topic and create rdd from now on regular pyspark code
-  
-    kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
-    lines = kvs.map(lambda x: x[1])
-    counts = lines.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
+    lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
+    counts = lines.flatMap(lambda line: line.split(" "))\
+                  .map(lambda word: (word, 1))\
+                  .reduceByKey(lambda a, b: a+b)
     counts.pprint()
-
-#start the streaming process.
 
     ssc.start()
     ssc.awaitTermination()
